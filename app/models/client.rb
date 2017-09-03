c60abf72338c0ea8536187b67413411c18f3e93f
Andre Costa <mr.luizandre@gmail.com>
2017-09-03 09:22:45 -0300

    loan styled

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
 class Client < ApplicationRecord
 	has_many :phones
+	has_many :loans
+	validates :name, presence: true
 
 	accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,8 @@
 class Loan < ApplicationRecord
   belongs_to :client
+  validates :value, :payment_day, :interest_rate, presence: true
+
+  def interest
+  	value * interest_rate/100
+  end
 end
